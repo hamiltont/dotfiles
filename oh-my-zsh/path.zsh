16e15de3639c0173d6ac5c41a7a3bb2ef150464c
Hamilton Turner <hamiltont@gmail.com>
2014-06-04 14:45:26 -0400

    Load OMZ in proper order
    We need to load OMZ’s config file, then OMZ, and then
    load any other topic ZSH files. I’ve done a bit of
    hackery to ensure this happens.
    1) The configuration file for OMZ sets up and then
    loads OMZ, so it’s always loaded with config set
    2) This file has been renamed path.zsh to ensure that
    it loads before any of the other topic.zsh files. It’s not
    technically a path modification, but I doubt anyone would
    have path problems due to this (unless their OMZ is really
    wacky)

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -3,11 +3,24 @@
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 # ZSH_THEME="kphoen"
-ZSH_THEME="avit"
+# ZSH_THEME="avit"
+ZSH_THEME="darkblood"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git docker common-aliases git-extras python rand-quote sbt scala screen svn wd)
+# plugins=(git docker common-aliases git-extras python rand-quote sbt scala screen svn wd)
 # plugins=(bower colorize brew-cask composer gem git-extras npm osx terminalapp vagrant autojump brew git-flow tmux)
+
+# Load oh-my-zsh (first, so topic.zsh files override this)
+$DEBUG && echo "Loading $ZSH/oh-my-zsh.sh"
+if [[ -a $ZSH/oh-my-zsh.sh ]]
+then
+  source "$ZSH/oh-my-zsh.sh"
+else
+  echo "Oh-my-zsh not found, downloading"
+  git clone https://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
+  source "$ZSH/oh-my-zsh.sh"
+fi
+
 
